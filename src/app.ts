9e071f18e76b4cd4c95cff362294ab1f99f0b8f3
MrAngalo <guilherme.carvalho7@hotmail.com>
2024-08-13 12:49:52 -0300

    created test message route

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
 import express from 'express';
+import bodyParser from 'body-parser';
+import { messageRoute } from './message';
+
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+app.post('/message', bodyParser.json(), messageRoute);
+app.use('/avatar', express.static('avatar.png'))
 
 app.listen(port, () => {
   return console.log(`Express is listening at http://localhost:${port}`);
---
+++ src/message.ts
@@ -0,0 +1,22 @@
+import { Request, Response } from "express";
+
+type MessageBody = {
+    "attachments": any[],
+    "avatar_url": string,
+    "created_at": number,
+    "group_id": string,
+    "id": string,
+    "name": string,
+    "sender_id": string,
+    "sender_type": string,
+    "source_guid": string,
+    "system": boolean,
+    "text": string,
+    "user_id": string
+  }
+
+export function messageRoute(req:Request, res:Response) {
+    const body = req.body as MessageBody;
+    console.log(body);
+    res.send('Hello World!');
+}
